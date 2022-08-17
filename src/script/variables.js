2237c966f108cd2153cd4db09ba6d66688bf9ead
Aleksei <lexa24032@gmail.com>
2022-08-17 12:15:38 +0300

    other variables are set

--- src/script/variables.js
+++ src/script/variables.js
@@ -1,1 +1,5 @@
 export const list = document.querySelector('ul');
+export const titleInput = document.querySelector('#title');
+export const bodyInput = document.querySelector('#body');
+export const form = document.querySelector('form');
+export const btn = document.querySelector('form button');
