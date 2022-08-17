79965b968845e8870dcedc0817def0253544ef32
Aleksei <lexa24032@gmail.com>
2022-08-17 12:08:11 +0300

    an important event listener is set

--- src/script/makeDB.js
+++ src/script/makeDB.js
@@ -8,4 +8,20 @@ openRequest.addEventListener('error', (err) => {
 
 openRequest.addEventListener('success', () => {
   console.log('Database opened successfully');
+
+  db = openRequest.result;
+});
+
+openRequest.addEventListener('upgradeneeded', (e) => {
+  db = e.target.result;
+
+  const objectStore = db.createObjectStore('notes_store', {
+    keyPath: 'id',
+    autoincrement: true,
+  });
+
+  objectStore.createIndex('title', 'title', { unique: false });
+  objectStore.createIndex('body', 'body', { unique: false });
+
+  console.log('Database setup is complete');
 });
