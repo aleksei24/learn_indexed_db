5bde6f349024c374b87c915a73c631eb04662254
Aleksei <lexa24032@gmail.com>
2022-08-17 11:57:22 +0300

    if it is a success, inform

--- src/script/makeDB.js
+++ src/script/makeDB.js
@@ -1,6 +1,11 @@
 let db;
 
 const openRequest = window.indexedDB.open('notes_DB', 1);
+
 openRequest.addEventListener('error', (err) => {
   console.error(err, 'Database is failed to open');
+});
+
+openRequest.addEventListener('success', () => {
+  console.log('Database opened successfully');
 });
