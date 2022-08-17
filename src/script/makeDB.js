b06e4b5a0a58014bd109ca65c04dd0d9e68db5f1
Aleksei <lexa24032@gmail.com>
2022-08-17 16:27:50 +0300

    request to open the DB is taken

--- src/script/makeDB.js
+++ src/script/makeDB.js
@@ -1,1 +1,14 @@
 import { form, btn, titleInput, bodyInput, list } from './variables';
+
+let db;
+
+const openRequest = window.indexedDB.open('test_DB', 1);
+
+openRequest.addEventListener('error', () => {
+  console.error('Database failed to open');
+});
+
+openRequest.addEventListener('success', () => {
+  console.log('Database opened successfully');
+  db = openRequest.result;
+});
