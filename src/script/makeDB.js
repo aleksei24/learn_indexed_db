75ec4c1a008f5a3bf9a2da9a39b11f41bfc2c115
Aleksei <lexa24032@gmail.com>
2022-08-17 16:42:55 +0300

    function addData is set

--- src/script/makeDB.js
+++ src/script/makeDB.js
@@ -12,3 +12,38 @@ openRequest.addEventListener('success', () => {
   console.log('Database opened successfully');
   db = openRequest.result;
 });
+
+openRequest.addEventListener('upgradeneeded', (e) => {
+  db = e.target.result;
+
+  const objectStore = db.createObjectStore('test_object_store', {
+    keyPath: 'id',
+    autoIncrement: true,
+  });
+  objectStore.createIndex('title', 'title', { unique: false });
+  objectStore.createIndex('body', 'body', { unique: false });
+
+  console.log('Database setup complete');
+});
+
+function addData(e) {
+  e.preventDefault();
+
+  const newItem = { title: titleInput.value, body: bodyInput.value };
+  const transaction = db.transaction(['test-object_store'], 'readwrite');
+  const objectStore = transaction.objectStore('test_object_store');
+  const addRequest = objectStore.add(newItem);
+
+  addRequest.addEventListener('success', () => {
+    titleInput.value = '';
+    bodyInput.value = '';
+  });
+
+  transaction.addEventListener('complete', () => {
+    console.log('Transaction completed');
+  });
+
+  transaction.addEventListener('error', (err) => {
+    console.error('Transaction not opened due to error: ', err);
+  });
+}
