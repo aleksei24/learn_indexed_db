798c249ec9b00a0e35a9049dd8ba26b93806add6
Aleksei <lexa24032@gmail.com>
2022-08-18 18:53:54 +0300

    production is made

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,1 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostCssPresetEnv = require('postcss-preset-env');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+
+  entry: './src/index.js',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'production'),
+    clean: {
+      dry: true,
+    },
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [PostCssPresetEnv()],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'To-Do App',
+      favicon: './src/fav/fav.png',
+      template: path.resolve(__dirname, './src/template.html'),
+    }),
+  ],
+};
